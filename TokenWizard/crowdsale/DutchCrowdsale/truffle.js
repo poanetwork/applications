c0ba1d0658f924469015e1a145448d66d433ff7a
Victor Baranov <baranov.viktor.27@gmail.com>
2018-10-24 19:11:16 +0300

    POA and xDai configs for truffle

--- TokenWizard/crowdsale/DutchCrowdsale/truffle.js
+++ TokenWizard/crowdsale/DutchCrowdsale/truffle.js
@@ -1,5 +1,7 @@
 const HDWalletProvider = require("truffle-hdwallet-provider-privkey");
 const privKeys = require('../pwd.js') // private keys
+const xDaiProvider = new HDWalletProvider(privKeys, "https://dai.poa.network/");
+const POAProvider = new HDWalletProvider(privKeys, "https://core.poa.network/");
 const SokolProvider = new HDWalletProvider(privKeys, "https://sokol.poa.network/");
 const KovanProvider = new HDWalletProvider(privKeys, "https://kovan.infura.io/");
 const RinkebyProvider = new HDWalletProvider(privKeys, "https://rinkeby.infura.io/");
@@ -12,6 +14,18 @@ module.exports = {
       port: 8545,
       gas: 6600000,
       network_id: "*" // Match any network id
+    },
+    xdai: {
+      provider: function() {
+        return xDaiProvider
+      },
+      network_id: '100',
+    },
+    poa: {
+      provider: function() {
+        return POAProvider
+      },
+      network_id: '99',
     },
     sokol: {
       provider: function() {
